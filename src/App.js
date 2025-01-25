11b98675ffc88a45ececfc5d3605a6d5867b02f1
salohiddinovAsadbek <mrxcapitain@gmail.com>
2025-01-25 14:05:08 +0500

    oas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import Abubakr from "./components/abubakr";
 
 function App() {
-  return <div className="App">
-    <Abubakr/>
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/components/asadbek.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function asadbek() {
-  return <div>asadbek</div>;
-}
-
-export default asadbek;
